56ee0ba401f95f0c91bf940400c2a3b271c25fb2
Felipe Rivas <rivascf@gmail.com>
2019-09-02 02:28:30 +0100

    Initial demo working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,69 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
+unsigned long delayTime;
+
+void printValues();
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial)
+    ; // time to get serial running
+  Serial.println(F("BME280 test"));
+
+  unsigned status;
+
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin();
+  if (!status)
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    Serial.print("SensorID was: 0x");
+    Serial.println(bme.sensorID(), 16);
+    Serial.print("   ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("   ID of 0x60 represents a BME 280.\n");
+    Serial.print("   ID of 0x61 represents a BME 680.\n");
+    while (1)
+      ;
+  }
+  Serial.print("SensorID is: 0x");
+  Serial.println(bme.sensorID(), 16);
+
+  Serial.println("-- Default Test --");
+  delayTime = 5000;
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  printValues();
+  delay(delayTime);
+}
+
+void printValues()
+{
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.println();
 }
