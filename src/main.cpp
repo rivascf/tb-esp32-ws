5045fabf4fc0ccde407e8786cfb604d3809b5edf
Felipe Rivas <rivascf@gmail.com>
2019-09-02 03:57:06 +0100

    Added TB functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,36 @@
 #include <Arduino.h>
+#include <ArduinoHttpClient.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include <Wire.h>
+#include <WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <ThingsBoard.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define SSID "HOME-8F16-2"
+#define SSID_PASSWORD "45164309"
+#define TBTOKEN "IEwjI1mYcMvHHEmTSDNx"
 
 Adafruit_BME280 bme;
 unsigned long delayTime;
+char thingsboardServer[] = "iotwit.info";
+WiFiClient wifiClient;
+ThingsBoard tb(wifiClient);
+int status = WL_IDLE_STATUS;
+unsigned long lastSend;
 
-void printValues();
+void getAndSendTemperatureAndHumidityData();
+void InitWiFi();
+void reconnect();
 
 void setup() {
   Serial.begin(115200);
   while (!Serial)
     ; // time to get serial running
+
+  
   Serial.println(F("BME280 test"));
 
   unsigned status;
@@ -37,17 +54,31 @@ void setup() {
   Serial.print("SensorID is: 0x");
   Serial.println(bme.sensorID(), 16);
 
+  lastSend = 0;
   Serial.println("-- Default Test --");
   delayTime = 5000;
   Serial.println();
+  delay(10);
+  InitWiFi();
 }
 
 void loop() {
-  printValues();
-  delay(delayTime);
+/*  printValues();
+  delay(delayTime);*/
+  if ( !tb.connected() ) {
+    reconnect();
+  }
+
+  if ( millis() - lastSend > 1000 ) { // Update and send only after 1 seconds
+    getAndSendTemperatureAndHumidityData();
+    lastSend = millis();
+  }
+
+  tb.loop();
+
 }
 
-void printValues()
+/*void printValues()
 {
   Serial.print("Temperature = ");
   Serial.print(bme.readTemperature());
@@ -67,4 +98,68 @@ void printValues()
   Serial.println(" %");
 
   Serial.println();
+}*/
+
+void getAndSendTemperatureAndHumidityData()
+{
+  Serial.println("Collecting temperature data.");
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float humidity = bme.readHumidity();
+  // Read temperature as Celsius (the default)
+  float temperature = bme.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from BME 280 sensor!");
+    return;
+  }
+
+  Serial.println("Sending data to ThingsBoard:");
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" *C ");
+
+  tb.sendTelemetryFloat("temperature", temperature);
+  tb.sendTelemetryFloat("humidity", humidity);
+}
+
+void InitWiFi()
+{
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(SSID, SSID_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!tb.connected()) {
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) {
+      WiFi.begin(SSID, SSID_PASSWORD);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+      Serial.println("Connected to AP");
+    }
+    Serial.print("Connecting to ThingsBoard node ...");
+    if ( tb.connect(thingsboardServer, TBTOKEN) ) {
+      Serial.println( "[DONE]" );
+    } else {
+      Serial.print( "[FAILED]" );
+      Serial.println( " : retrying in 5 seconds]" );
+      // Wait 5 seconds before retrying
+      delay( 5000 );
+    }
+  }
 }
